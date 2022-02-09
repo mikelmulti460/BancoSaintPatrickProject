69d656098e1d35a57752f6fc8e1855f41ac0f583
mikelmulti460 <mikelenrique@hotmail.com>
2022-02-09 18:47:34 -0500

    modificado el modelo cliente

--- API/API/settings.py
+++ API/API/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+AUTH_USER_MODEL = 'clients_api.UserClient'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- API/clients_api/models.py
+++ API/clients_api/models.py
@@ -1,16 +1,38 @@
-import email
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 class UserClientManager(BaseUserManager):
-    pass
+    """Manager for Users Client"""
+
+    def create_user(self, email, name, password=None):
+        """Create New User Client"""
+        if not email:
+            raise ValueError('El usuario debe tener un Email')
+        
+        email = self.normalize_email(email)
+        user = self.model()
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self._db)
+
+        return user
 
 class UserClient(AbstractBaseUser, PermissionsMixin):
     """ Model Data Base for Users in the System """
     email = models.EmailField(max_length=80, unique=True)
     name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
